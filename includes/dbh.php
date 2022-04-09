8da27649fa88e03a67ef8422156e5056a03dcbe7
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2022-04-10 05:14:52 +0530

    adding trainer, includes

---
+++ includes/dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "15July@2002";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName );
